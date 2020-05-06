07a7f1ca334acc1ce3343cd212d5949d27b51175
Andriy Fuji Pendloski <afp@edu.univali.br>
2020-05-06 19:22:37 -0300

    Commented and less important functions moved to header

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <stack>
 #include <vector>
-#include <queue>
 #include "main.h"
 
 using namespace std;
@@ -11,52 +10,6 @@ using namespace std;
 #define WHITE        7
 #define GRAY         8
 #define BRIGHTWHITE  15
-
-int xyToPos(int x, int y)
-{
-    int pos = y * SIZE + x;
-
-    return pos;
-}
-
-//Returns true if the position is in the vector
-bool find(vector<int> vector, int x)
-{
-    for(int i = 0; i < vector.size(); i++)
-        if(vector[i] == x)
-            return true;
-    return false;
-}
-
-void showBoard(vector<int> path)
-{
-    int count = 0;
-    for(int i = 0; i < SIZE; i++)
-    {
-        for(int j = 0; j < SIZE; j++)
-        {
-            int aux = xyToPos(j, i);
-            if(count % 2 == 0)
-                textcolor(BLACK, WHITE);
-            else
-                textcolor(WHITE, BLACK);
-
-            if(!find(path, aux))
-                cout << ' ' << ' ' << ' ';
-            else
-            {
-                if(aux < 10)
-                    cout << ' ' << aux << ' ';
-                else
-                    cout << ' ' << aux;
-            }
-            count++;
-        }
-        cout << '\n';
-        //count++;
-    }
-    textcolor(WHITE, BLACK);
-}
 
 //Checks whether the move ends up inside or outside the board
 bool validMove(int x)
@@ -70,6 +23,7 @@ bool validMove(int x)
 //Generates possible knight moves and returns them in possibleMoves
 vector<int> genMoves(vector<int> path, int pos)
 {
+    //Possible moves the knight can do in a matrix
     int directions[16] = {-1, 2, 1, 2, 2, 1, 2, -1, 1, -2, -1, -2, -2, -1, -2, 1};
     int posX = pos % SIZE;
     int posY = pos / SIZE;
@@ -90,15 +44,18 @@ vector<int> genMoves(vector<int> path, int pos)
     return possibleMoves;
 }
 
-//Criar uma função para inserir em ordem no vetor
+//Rearanges the order of possibleMoves according to Warnsdorff's rule
 vector<int> prioritizeMoves(vector<int> path, vector<int> possibleMoves)
 {
+    //Since the first move picked is always the last position of possibleMoves, a stack is used
     stack<int> warnsdorffOrder;
+    //Stacks the positions with fewest moves first
     for(int i = 1; i < 9; i++)
     {
         for(int j = 0; j < possibleMoves.size(); j++)
         {
             vector<int> auxVector;
+            //Saves the moves in the next position
             auxVector = genMoves(path, possibleMoves[j]);
             if(auxVector.size() == i)
                 warnsdorffOrder.push(possibleMoves[j]);
@@ -106,6 +63,7 @@ vector<int> prioritizeMoves(vector<int> path, vector<int> possibleMoves)
     }
 
     vector<int> prioritizedVector;
+    //Since the positions with more moves were pushed last, they'll be inserted first in prioritizedVector
     while(!warnsdorffOrder.empty())
     {
         prioritizedVector.push_back(warnsdorffOrder.top());
@@ -114,36 +72,33 @@ vector<int> prioritizeMoves(vector<int> path, vector<int> possibleMoves)
     return prioritizedVector;
 }
 
-
-void showPath(vector<int> path)
-{
-    for(int i = 0; i < path.size(); i++)
-        cout << path[i] << ' ';
-}
-
-
 //Pos is the current position, path is the visited nodes
 bool knightsTour(int pos, vector<int> &path, int depth = 0)
 {
     bool completed;
     path.push_back(pos);
+    
     if(depth < SIZE*SIZE-1)
     {
         completed = false;
 
         vector<int> possibleMoves;
         possibleMoves = genMoves(path, pos);
+        //If it generated more than one move, reorganize them following Warnsdorff's rule
         if(possibleMoves.size()>1)
             possibleMoves = prioritizeMoves(path, possibleMoves);
 
+        //If it still has moves to try
         while(!possibleMoves.empty() && !completed)
         {
+            //Tries the last move in the vector
             pos = possibleMoves[possibleMoves.size()-1];
             completed = knightsTour(pos, path, depth+1);
+            //Move branch wasn't successful
             possibleMoves.pop_back();
         }
-
         if(!completed)
+            //
             path.pop_back();
     }
     else
@@ -154,19 +109,96 @@ bool knightsTour(int pos, vector<int> &path, int depth = 0)
 
 int main()
 {
-    /*cout << "Chess board" << "\t\t\t\t" << "Knight's path" << '\n';
+    header();
 
-    gotoxy(0, 1);
-    showBoards(board);*/
+    bool tryAgain;
+    do
+    {
+        //Prints the chessboard
+        system("cls");
+        gotoxy(0, 0);
+        printBoard();
 
-    vector<int> path;
-    knightsTour(1, path);
+        gotoxy(0, SIZE+2);
+        cout << "Enter the knight's starting position (e.g. a4, d6):\n";
 
+        char posX;
+        int posY;
+        bool exit;
+        //Validates the input
+        do
+        {
+            cin >> posX;
+            cin >> posY;
+            
+            posX = tolower(posX);
+            exit = true;
+            //Error if it's outside the chessboard range
+            if(posY < 1 || posY > 8 || posX < 'a' || posX > 'h')
+            {
+                system("cls");
+
+                printBoard();
+                gotoxy(0, SIZE+2);
+                cout << "Invalid position! Try again (e.g. a4, d6):\n";
+                exit = false;
+            }
+        }
+        while(!exit);
         
-    gotoxy(0, 3);
-    showPath(path);
+        vector<int> path;
+        //Solves the tour while saving it in path
+        knightsTour(algebraicToPos(posX, posY), path);
+        
+        //Prints the knight's movement step by step
+        gotoxy(0, 0);
+        for(int i = 0; i < SIZE*SIZE; i++)
+        {
+            int posX = path[i] % SIZE;
+            int posY = path[i] / SIZE;
 
-    cout << '\n';
-    system("pause");
+            //If it's a black or a while tile
+            if((2+posX*3+posY) % 2 == 0)
+                textcolor(BLACK, WHITE);
+            else
+                textcolor(WHITE, BLACK);
+            
+            //Calculates where it should print the movement
+            gotoxy(3+posX*3, posY);
+            cout << i+1;
+            
+            //Resets the color and moves the cursor down to not interrupt any prints
+            textcolor(WHITE, BLACK);
+            gotoxy(0, SIZE+6);
+            system("pause");
+        }
+        textcolor(WHITE, BLACK);
+
+        char choice;
+        //If the program should run again or close
+        do
+        {
+            gotoxy(0, SIZE+5);
+            cout << "Finished! Try again? (Y/N) ";
+            cin >> choice;
+            choice = tolower(choice);
+
+            exit = false;
+            //Only leaves the loop if Y or N are typed
+            if(choice == 'y')
+            {
+                tryAgain = true;
+                exit = true;
+            }
+            else if(choice == 'n')
+            {
+                tryAgain = false;
+                exit = true;
+            }
+        }
+        while(!exit);
+    }
+    while(tryAgain);
+    
     return 0;
 }
--- main.h
+++ main.h
@@ -1,5 +1,6 @@
 #include <windows.h>
 
+#define SIZE    8
 #define BLACK        0
 #define BLUE         1
 #define GREEN        2
@@ -17,6 +18,67 @@
 #define LIGHTYELLOW  14
 #define BRIGHTWHITE  15
 
+//Converts and returns an XY into a position ID (0 to 63)
+int xyToPos(int x, int y)
+{
+    int pos = y * SIZE + x;
+
+    return pos;
+}
+
+//Converts and returns an algebraic position into a position ID (0 to 63)
+int algebraicToPos(char c, int x)
+{
+    int auxY[8] = {7, 6, 5, 4, 3, 2, 1, 0};
+    int auxX;
+
+    switch(c)
+    {
+        case 'a':
+            auxX = 0;
+            break;
+        case 'b':
+            auxX = 1;
+            break;
+        case 'c':
+            auxX = 2;
+            break;
+        case 'd':
+            auxX = 3;
+            break;
+        case 'e':
+            auxX = 4;
+            break;
+        case 'f':
+            auxX = 5;
+            break;
+        case 'g':
+            auxX = 6;
+            break;
+        case 'h':
+            auxX = 7;
+            break;
+    }
+
+    return xyToPos(auxX ,auxY[x-1]);
+}
+
+//Prints a vector
+void showVector(std::vector<int> path)
+{
+    for(int i = 0; i < path.size(); i++)
+        std::cout << path[i] << ' ';
+}
+
+//Returns true if the position is in the vector
+bool find(std::vector<int> vector, int x)
+{
+    for(int i = 0; i < vector.size(); i++)
+        if(vector[i] == x)
+            return true;
+    return false;
+}
+
 //Jumps to position x,y in the cmd
 void gotoxy(int x, int y)
 {
@@ -30,4 +92,53 @@ void gotoxy(int x, int y)
 void textcolor(int text, int background)
 {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)(text | background << 4));
+}
+
+//Prints the chessboard
+void printBoard()
+{
+    int count = 0;
+    for(int i = 0; i < SIZE; i++)
+    {
+        std::cout << 8-i << ' ';
+        for(int j = 0; j < SIZE; j++)
+        {
+            if(count % 2 == 0)
+                textcolor(BLACK, WHITE);
+            else
+                textcolor(WHITE, BLACK);
+
+            std::cout << ' ' << ' ' << ' ';
+            count++;
+        }
+        std::cout << '\n';
+        count++;
+        textcolor(WHITE, BLACK);
+    }
+    char c = 'a';
+    std::cout << ' ';
+    for(int i = 0; i < SIZE; i++)
+    {
+        std::cout << "  " << c;
+        c++;
+    }
+}
+
+void header()
+{
+    gotoxy(0, 0);
+    textcolor(LIGHTYELLOW, BLACK);
+    std::cout << "Knight's tour";
+    gotoxy(0, 1);
+    textcolor(YELLOW, BLACK);
+    std::cout << "Engenharia de Computacao - UNIVALI";
+    gotoxy(0, 2);
+    std::cout << "Graphs";
+    gotoxy(0, 3);
+    std::cout << "Andriy Fujii Pendloski";
+
+    textcolor(BLACK, WHITE);
+    gotoxy(0, 5);
+    system("pause");
+    textcolor(BRIGHTWHITE, BLACK);
 }
